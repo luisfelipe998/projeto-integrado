779d0874053ef3f78b91b33993a7a81a21c63ef1
Luís Felipe <53181076+luisfelipe998@users.noreply.github.com>
2025-11-01 14:29:39 -0300

    add database connection (#29)

---
+++ src/infra/postgres/client.ts
@@ -0,0 +1,14 @@
+import postgres from 'postgres';
+
+export const sql = postgres({
+  host: process.env.DATABASE_HOST || 'localhost',
+  port: Number(process.env.DATABASE_PORT) || 5432,
+  database: process.env.DATABASE_NAME || 'library',
+  username: process.env.DATABASE_USER || 'admin',
+  password: process.env.DATABASE_PASSWORD || 'admin',
+  ssl: false
+});
+
+export async function connect() {
+  await sql`SELECT 1`;
+}
