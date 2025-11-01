0311a32a74bbcff6b11cfdadc55a58c0d8e5b66f
Luís Felipe <53181076+luisfelipe998@users.noreply.github.com>
2025-11-01 14:34:32 -0300

    add book usecases (#31)

---
+++ src/application/book/bookRepository.ts
@@ -0,0 +1,11 @@
+import { Book } from "./book";
+
+export interface BookRepository {
+    getAllBooks(status: string): Promise<Book[]>;
+    getBooksByTitle(title: string, status: string): Promise<Book[]>;
+    getBooksByISBN(isbn: string, status: string): Promise<Book[]>;
+    getBookById(id: number): Promise<Book | null>;
+    createBook(book: Partial<Book>): Promise<number>;
+    updateBook(id: number, book: Partial<Book>): Promise<number | null>;
+    deleteBook(id: number): Promise<boolean>;
+}
